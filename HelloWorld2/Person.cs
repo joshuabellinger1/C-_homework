ef6384708308d98359c6b0ed67edae492d834ae8
Joshua Bellinger <Josh@Jeanettes-MacBook-Air.local>
2023-12-04 17:38:09 -0600

    first commit

---
+++ HelloWorld2/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HelloWorld2
+{
+	public class Person
+	{
+		public string FirstName;
+		public string LastName;
+
+		public void Introduce()
+		{
+			Console.WriteLine("My name is " + FirstName + " " + LastName);
+		}
+
+
+	}
+}
+
---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter the width of the image.");
+        int width = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter the height of the image.");
+        int height = int.Parse(Console.ReadLine());
+
+        if (width > height)
+            Console.WriteLine("Landscape");
+        else
+            Console.WriteLine("Portrait");
+
+    }
+}
