f79f7cc122ef60c01aadf16a3b89606d2204fe2d
Joshua Bellinger <Josh@Jeanettes-MacBook-Air.local>
2023-12-04 19:01:33 -0600

    Added error handling and removed unused file

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -4,15 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please enter the width of the image.");
-        int width = int.Parse(Console.ReadLine());
-        Console.WriteLine("Please enter the height of the image.");
-        int height = int.Parse(Console.ReadLine());
+        int width;
+        int height;
+        bool isValid;
+
+        do {
+            Console.WriteLine("Please enter the width of the image.");
+            isValid = int.TryParse(Console.ReadLine(), out width);
+            if (!isValid)
+                Console.WriteLine("You are a big dummy. That's not a number");
+            else
+                break;
+        } while (true);
+
+        do {
+            Console.WriteLine("Please enter the height of the image.");
+            isValid = int.TryParse(Console.ReadLine(), out height);
+            if (!isValid)
+                Console.WriteLine("You are a big dummy. That's not a number");
+            else
+                break;
+        } while (true);
 
         if (width > height)
             Console.WriteLine("Landscape");
         else
             Console.WriteLine("Portrait");
-
     }
 }
